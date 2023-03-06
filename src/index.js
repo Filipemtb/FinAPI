4939d0a35aa2941fb29bb95e8597a2de2112e874
Filipe Mota <99206125+Filipemtb@users.noreply.github.com>
2023-03-06 18:30:46 -0300

    creating account record

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
+const { response } = require("express");
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+
+app.use(express.json());
+
+const customers = [] 
+
+app.post("/account", (request, response) => {
+   const { cpf, name} = request.body;
+
+   customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+   });
+   return response.status(201).send();
+});
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT} `));
