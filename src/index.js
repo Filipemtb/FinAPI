cb21cd7a50b377ebeee6b7b54f46e2d4579b27e5
Filipe Mota <99206125+Filipemtb@users.noreply.github.com>
2023-03-06 19:15:43 -0300

    creating middleware

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const { response } = require("express");
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 
@@ -6,7 +5,22 @@ const app = express();
 
 app.use(express.json());
 
-const customers = [] 
+const customers = [];
+
+function verifyIfExistsAccountCPF (request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if(!customer) {
+    return response.status(400).json({ error: "Customer not found"})
+  }
+
+  request.customer = customer;
+
+  return next();
+
+}
 
 app.post("/account", (request, response) => {
    const { cpf, name} = request.body;
@@ -26,14 +40,8 @@ app.post("/account", (request, response) => {
    return response.status(201).send();
 });
 
-app.get("/statement", (request, response) => {
-  const { cpf } = request.headers;
-
-  const customer = customers.find(customer => customer.cpf === cpf);
-
-  if(!customer) {
-    return response.status(400).json({ error: "Customer not found"})
-  }
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
 
   return response.json(customer.statement);
 });
